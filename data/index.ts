f98168e2e8a2f8d54ad6a5bc639ffbc44cacd7bc
Simon Maigrot <simon.maigrot33@gmail.com>
2025-03-18 19:24:36 +0100

    feat: Adding new Portfolio

---
+++ data/index.ts
@@ -0,0 +1,191 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a SAAS named Formagold",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Formagold",
+    des: "FormaGold – The Business Coaching Marketplace for Entrepreneurs & Experts.",
+    img: "/formagold.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://formagold.fr",
+  },
+  {
+    id: 2,
+    title: "Wakatépé",
+    des: "Wakatépé – Empowering Students in France to Find Their Educational Pathways.",
+    img: "/wakatepe.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://wakatepe.fr",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Simon was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Simon's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Simon is the ideal partner.",
+    name: "Remi Maigrot",
+    title: "Student at Epitech",
+  },
+  {
+    quote:
+      "Simon quickly understood our needs and provided effective solutions. His technical expertise and attention to detail were key assets in successfully delivering our project. Working with him was a smooth and productive experience. I highly recommend him for any demanding web project.",
+    name: "Damien Barthe",
+    title: "Works in renewable energy",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Web Developer Intern - Wakatépé",
+    desc: "Completed a 6-month internship at Wakatépé, contributing to the development of a web platform using modern technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Co-founder - FormaGold",
+    desc: "Co-founded FormaGold, a SaaS marketplace for business-oriented online courses, connecting experts with learners.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "DevOps Learning Project - Chocolatine",
+    desc: "Developed 'Chocolatine', a project designed to explore GitHub Actions and DevOps automation workflows.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Automation & Workflow Optimization - Popeye",
+    desc: "Built 'Popeye' to automate repetitive tasks using GitHub Actions, improving workflow efficiency.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/SimonMaigrot"
+  },
+  {
+    id: 2,
+    img: "/fiverr.svg",
+    link: "https://fr.fiverr.com/s/akB9VyK"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "www.linkedin.com/in/simon-maigrot-73254b29a"
+  },
+  {
+    id: 4,
+    img: "/malt.svg",
+    link: "https://www.malt.fr/profile/simonmaigrot"
+  }
+];
--- nuxt.config.ts
+++
@@ -1,7 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  css: ['~/assets/css/global.css'],
-  modules: ['@nuxtjs/tailwindcss']
-})
