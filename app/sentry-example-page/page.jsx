14195476291407393bc26ad551009caf45c8e8e1
Simon Maigrot <simon.maigrot33@gmail.com>
2025-03-18 20:00:33 +0100

    hotfix: Fixing sentry errors

--- app/sentry-example-page/page.jsx
+++ app/sentry-example-page/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Head from "next/head";
-import * as Sentry from "@sentry/nextjs";
 
 export default function Page() {
   return (
@@ -47,17 +46,6 @@ export default function Page() {
             color: "white",
             fontSize: "14px",
             margin: "18px",
-          }}
-          onClick={() => {
-            Sentry.startSpan({
-              name: 'Example Frontend Span',
-              op: 'test'
-            }, async () => {
-              const res = await fetch("/api/sentry-example-api");
-              if (!res.ok) {
-                throw new Error("Sentry Example Frontend Error");
-              }
-            });
           }}
         >
           Throw error!
